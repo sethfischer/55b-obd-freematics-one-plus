79a374e3b759deaad0585b29076e0c9e533fbbc5
Seth Fischer <seth@fischer.nz>
2020-08-23 17:22:03 +1200

    Read device voltage

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,18 @@
 
 #include <Arduino.h>
 #include <BluetoothSerial.h>
-#include <FreematicsPlus.h>
 
 #include "Debug/Trace.h"
+#include "FreematicsPlus.h"
 
 #ifdef DEBUG
 BluetoothSerial SerialBT;
 #endif
+
+COBD obd;
+FreematicsESP32 sys;
+
+bool connected = false;
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
@@ -28,9 +33,51 @@ void setup() {
 
   DEBUG_TRACE("[INFO] setup() start\n")
 
+  DEBUG_TRACE("[INFO] Initialising OBD\n")
+  while (!sys.begin())
+    ;
+  obd.begin(sys.link);
+
+  digitalWrite(PIN_LED, LOW);
+
   DEBUG_TRACE("[INFO] setup() end\n")
 }
 
-void loop() {}
+void loop() {
+  DEBUG_TRACE("[INFO] loop() start\n")
+
+  digitalWrite(PIN_LED, HIGH);
+
+  if (!connected) {
+    digitalWrite(PIN_LED, HIGH);
+    DEBUG_TRACE("[INFO]Connecting to OBD...")
+    if (obd.init(PROTO_CAN_11B_500K)) {
+      DEBUG_TRACE(" success\n")
+      connected = true;
+    } else {
+      DEBUG_TRACE(" failed\n")
+    }
+    digitalWrite(PIN_LED, LOW);
+    return;
+  } else {
+    DEBUG_TRACE("[INFO] Connected to OBD\n")
+  }
+
+  DEBUG_TRACE("[INFO] Device voltage: ");
+  DEBUG_TRACE(obd.getVoltage());
+  DEBUG_TRACE(" V\n");
+
+  if (obd.errors > 2) {
+    DEBUG_TRACE("[WARN] OBD errors. Resetting.\n");
+    connected = false;
+    obd.reset();
+  }
+
+  digitalWrite(PIN_LED, LOW);
+
+  DEBUG_TRACE("[INFO] loop() end\n")
+
+  delay(2000);
+}
 
 #endif
